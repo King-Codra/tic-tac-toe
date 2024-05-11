14a2e0017e071c1f0f70e23c28a6888e7afa919d
Tjalling <tjalvh@gmail.com>
2024-05-11 12:01:43 +0200

    Good starting off point, no logic yet implemented

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,14 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
-    // Add options here
+    sassOptions: {
+      extension: 'scss',
+    },
+
+    'ember-bootstrap': {
+      bootstrapVersion: 5,
+      importBootstrapCSS: false,
+    },
   });
 
   return app.toTree();
